53637e43671b0ae5a2d0fc11cacf85a4f270680e
Ilya <ilromka@mail.ru>
2024-11-19 19:43:20 +0300

    APP

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="app">
+        APP
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+    };
+</script>
+
+<style lang="scss" scoped>
+    .app {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        overflow: hidden;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
